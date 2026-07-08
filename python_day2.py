0fec108402895478bc409dbbedf16eff06c23530
Tejas Khare <tejaskhare360@gmail.com>
2026-07-09 02:21:20 +0530

    Add files via upload

--- python_day2.py
+++ python_day2.py
@@ -10,3 +10,23 @@ print(type(c))
 print(type(d))
 print(type(e))
 print(type(f))
+
+
+#this is a comment
+'''
+This is a multi-line comment
+'''
+#operators
+
+a = 20
+b = 60
+sum = a+b
+diff= a-b
+mult= a*b
+div= a/b
+reminder= a%b #this is for finding reminders. 
+print(sum)
+print(diff)
+print(mult)
+print(div)
+print(reminder)

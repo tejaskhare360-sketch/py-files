555b10a2423a0f496d71f0d3fe87e7c2d3fb2b69
Tejas Khare <tejaskhare360@gmail.com>
2026-07-10 00:53:51 +0530

    Add files via upload

---
+++ python_day3.py
@@ -0,0 +1,17 @@
+a = 100
+b = 50
+print(a > b) #True 
+print(a < b) #False
+print(a == b)#False
+print(a != b) #True
+
+
+#checking type converstions. 
+
+x = 10
+y = 20.5
+print(x+y)
+
+m,n = 20, 30.5
+
+print(n+m)

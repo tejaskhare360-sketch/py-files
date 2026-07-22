587ef440ee99b61700b36cfdabc6bd0c0abafefe
Tejas Khare <tejaskhare360@gmail.com>
2026-07-22 19:39:27 +0530

    Add files via upload

---
+++ lect2_22july.py
@@ -0,0 +1,12 @@
+string = "hello, world" #a string 
+#for printing in other line. 
+
+str2 = "my name is \n batman"
+
+print(str2)
+print(len(str2)) #length of string
+
+#indexing 
+print(str2[0])
+
+

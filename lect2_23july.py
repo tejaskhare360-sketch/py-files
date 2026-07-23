95e73ca6be47b31317089ca22787ef534f2c3a3d
Tejas Khare <tejaskhare360@gmail.com>
2026-07-23 23:41:34 +0530

    Add files via upload

---
+++ lect2_23july.py
@@ -0,0 +1,19 @@
+x = "batman"
+#ends with fuction 
+print(x.endswith("ana")) #false
+print(x.endswith("man")) #true
+
+#capitlaize function 
+print(x.capitalize())
+print(x.capitalize()) 
+
+#replace function 
+
+print(x.replace("bat", "cat")) #replaces bat with cat
+
+#-------------------------------------------------------#
+
+y = "i am studying python in my dungeon" 
+
+#replace function 
+print(y.replace("python", "javascript")) #replaces python with javascript
